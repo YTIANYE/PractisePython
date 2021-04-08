83e013f7e13087e6844ecbfe2e7d303fa4d09c8c
YTIANYE <13293209977@163.com>
2021-04-08 17:32:38 +0800

    相关题 只出现一次的数字 异或解决

---
+++ test_PyTorch.py
@@ -0,0 +1,8 @@
+import torch
+import torchvision
+
+
+print(torch)
+
+x = torch.rand(5,3)
+print(x)
--- test_list.py
+++ test_list.py
@@ -24,10 +24,11 @@ def testNulllist(arr1):
 
 # 测试初始化数组
 def testInitarr():
-    arr = [1 * 8]   # [8]
+    arr = [1 * 8]  # [8]
     print(arr)
     arr2 = [1] * 8  # [1, 1, 1, 1, 1, 1, 1, 1]
     print(arr2)
+
 
 # 测试 连续的list赋值 与 连续的变量赋值
 def testListandInt():
@@ -40,6 +41,11 @@ def testListandInt():
     print(arr, arr1)
     print(a, b)
 
+# 测试 sorted 函数
+def testsorted():
+    nums = [1, 0, 1]
+    print(sorted(nums))
+
 
 """
     2.  关于循环的测试
@@ -49,24 +55,30 @@ def testListandInt():
 # 测试for循环左闭右开的设定 在步长为-1时是否有效
 def testFor():
     arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    for i in range(len(arr), -1, -1):   # 左闭右开，不会打印 -1
+    for i in range(len(arr), -1, -1):  # 左闭右开，不会打印 -1
         # print(arr[i])   # 会报错 IndexError: list index out of range
         print(i, end=" ")  # 10 9 8 7 6 5 4 3 2 1 0
     print()
 
     # 测试数组初始化中包含 for 语句
     count = [0 for i in range(5)]
-    print("count:", count)    # output: [0, 0, 0, 0, 0]
+    print("count:", count)  # output: [0, 0, 0, 0, 0]
     ans = ["" for _ in arr]
-    print("ans:", ans)      # ans: ['', '', '', '', '', '', '', '', '', '']
+    print("ans:", ans)  # ans: ['', '', '', '', '', '', '', '', '', '']
 
+
+# 测试反向循环访问时的区间
+def testListSquare():
+    # 反向循环的时候，注意第一个第二个参数与正向循环位置不同
+    for i in range(5, 0, -1):   # 左闭右开
+        print(i)
 
 
 if __name__ == "__main__":
     # testList()
     # testListandInt()
     # testFor()
-    testInitarr()
+    # testInitarr()
+    # testListSquare()
+    testsorted()    # 由小到大排序
     pass
-
-
