44154553704a66171c44a8a7da8d2a4c8bb0dd21
YTIANYE <13293209977@163.com>
2021-07-05 10:18:11 +0800

    反转链表（迭代、递归）

---
+++ 初级算法-帮助入门/04链表/反转链表.py
@@ -0,0 +1,103 @@
+"""
+给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+
+输入：head = [1,2,3,4,5]
+输出：[5,4,3,2,1]
+
+输入：head = [1,2]
+输出：[2,1]
+
+输入：head = []
+输出：[]
+
+进阶：链表可以选用迭代或递归方式完成反转。你能否用两种方法解决这道题？
+"""
+from typing import List
+
+
+class ListNode:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+
+# 打印链表
+def printLinkList(head: ListNode):
+    cur = head
+    while cur is not None:
+        print(cur.val, end=" ")
+        cur = cur.next
+    print("\n")
+
+
+# 由数组创建链表
+def arrToLinkList(arr: List[int]) -> ListNode:
+    if not arr:
+        return None
+    head = ListNode(arr[0])
+    cur = head
+    for i in range(1, len(arr)):
+        cur.next = ListNode(arr[i])
+        cur = cur.next
+    return head
+
+
+class Solution:
+    """
+    执行用时：52 ms, 在所有 Python3 提交中击败了20.33%的用户
+    内存消耗：15.6 MB, 在所有 Python3 提交中击败了43.16%的用户
+    """
+
+    # 我的题解：头插法，迭代
+    def reverseList1(self, head: ListNode) -> ListNode:
+        if head is None:
+            return head
+        else:
+            yummy = ListNode(0)
+            yummy.next = head
+            pre = head
+            cur = head.next
+            while cur is not None:
+                pre.next = cur.next
+                cur.next = yummy.next
+                yummy.next = cur
+                cur = pre.next
+        return yummy.next
+
+    """
+    执行用时：52 ms, 在所有 Python3 提交中击败了20.33%的用户
+    内存消耗：19.6 MB, 在所有 Python3 提交中击败了16.57%的用户
+    """
+
+    # 我的题解：头插法： 其实不是递归，还是迭代
+    def reverseList2(self, head: ListNode) -> ListNode:
+        if head is None:
+            return head
+        else:
+            return self.recursion(head, head)
+
+    def recursion(self, head: ListNode, pre: ListNode) -> ListNode:
+        if pre.next is None:
+            return head
+        else:
+            cur = pre.next
+            pre.next = cur.next
+            cur.next = head
+            head = cur
+            return self.recursion(head, pre)
+
+    # 官方题解：递归
+    def reverseList(self, head: ListNode) -> ListNode:
+        if head is None or head.next is None:
+            return head
+        newNode = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        return newNode
+
+
+s = Solution()
+arr = [1, 2, 3, 4]
+head = arrToLinkList(arr)
+printLinkList(head)
+printLinkList(s.reverseList(head))
