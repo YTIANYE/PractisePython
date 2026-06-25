dff25794781fe258d8929de48deed4985c4eee1a
Yao Tianye <13293209977@163.com>
2026-06-26 00:34:00 +0800

    102. 二叉树的层序遍历

---
+++ LeetCode/102. 二叉树的层序遍历.py
@@ -0,0 +1,53 @@
+"""
+给你二叉树的根节点 root ，返回其节点值的 层序遍历 。 （即逐层地，从左到右访问所有节点）。
+
+ 
+
+示例 1：
+
+
+输入：root = [3,9,20,null,null,15,7]
+输出：[[3],[9,20],[15,7]]
+示例 2：
+
+输入：root = [1]
+输出：[[1]]
+示例 3：
+
+输入：root = []
+输出：[]
+ 
+
+提示：
+
+树中节点数目在范围 [0, 2000] 内
+-1000 <= Node.val <= 1000
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        res = []
+        if root is None:
+            return res 
+        que = [root]
+        while que:
+            q = que 
+            que = []
+            values = []
+            while q:
+                node = q.pop(0)
+                values.append(node.val)
+                if node.left :
+                    que.append(node.left)
+                if node.right:
+                    que.append(node.right)
+            res.append(values)
+        return res 
+        
+        
