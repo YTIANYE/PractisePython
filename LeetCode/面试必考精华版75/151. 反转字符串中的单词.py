e85b7f557adec87b0769fca2cc603bbb56cc2716
YTIANYE <13293209977@163.com>
2026-08-31 01:44:20 +0800

    151. 反转字符串中的单词

---
+++ LeetCode/面试必考精华版75/151. 反转字符串中的单词.py
@@ -0,0 +1,101 @@
+"""
+给你一个字符串 s ，请你反转字符串中 单词 的顺序。
+
+单词 是由非空格字符组成的字符串。s 中使用至少一个空格将字符串中的 单词 分隔开。
+
+返回 单词 顺序颠倒且 单词 之间用单个空格连接的结果字符串。
+
+注意：输入字符串 s中可能会存在前导空格、尾随空格或者单词间的多个空格。返回的结果字符串中，单词间应当仅用单个空格分隔，且不包含任何额外的空格。
+
+ 
+
+示例 1：
+
+输入：s = "the sky is blue"
+输出："blue is sky the"
+示例 2：
+
+输入：s = "  hello world  "
+输出："world hello"
+解释：反转后的字符串中不能存在前导空格和尾随空格。
+示例 3：
+
+输入：s = "a good   example"
+输出："example good a"
+解释：如果两个单词间有多余的空格，反转后的字符串需要将单词间的空格减少到仅有一个。
+ 
+
+提示：
+
+1 <= s.length <= 104
+s 包含英文大小写字母、数字和空格 ' '
+s 中 至少存在一个 单词
+ 
+
+进阶：如果字符串在你使用的编程语言中是一种可变数据类型，请尝试使用 O(1) 额外空间复杂度的 原地 解法。
+"""
+# 官方题解：双端队列
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        left, right = 0, len(s) - 1
+        # 去掉字符串开头的空白字符
+        while left <= right and s[left] == ' ':
+            left += 1
+        
+        # 去掉字符串末尾的空白字符
+        while left <= right and s[right] == ' ':
+            right -= 1
+            
+        d, word = collections.deque(), []
+        # 将单词 push 到队列的头部
+        while left <= right:
+            if s[left] == ' ' and word:
+                d.appendleft(''.join(word))
+                word = []
+            elif s[left] != ' ':
+                word.append(s[left])
+            left += 1
+        d.appendleft(''.join(word))
+        
+        return ' '.join(d)
+
+
+# 我的题解：双指针。虽然能AC，但是复杂度\(O(n^2)\)
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        res = ""
+        n = len(s)
+        i = 0 
+        while i < n:
+            while i < n and s[i] == " ":
+                i += 1
+            if i >= n :
+                break 
+            j = i 
+            while j < n and s[j] != " ":
+                j += 1
+            temp_s = s[i:j]
+            res = temp_s + " " + res if res != "" else temp_s
+            i = j 
+        return res 
+
+# 豆包优化：列表存储
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        words = []
+        n = len(s)
+        i = 0
+        while i < n:
+            while i < n and s[i] == " ":
+                i += 1
+            if i >= n:
+                break
+            j = i
+            while j < n and s[j] != " ":
+                j += 1
+            words.append(s[i:j])
+            i = j
+        # 反转列表再join，列表操作均摊O(1)
+        return " ".join(reversed(words))    # 优雅的拼接方式
+
+        
